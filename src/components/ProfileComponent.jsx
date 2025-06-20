06b117808d3f5e928e5911f3f47fcbde81a1fbca
BhojGC <ghartich@sheridancollege.ca>
2025-06-20 01:41:28 -0400

    Code Clean Up & Document name change & logo added

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -8,7 +8,7 @@ import { useEffect, useRef, useState } from "react";
 const ProfileComponent = () => {
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const profileButtonRef = useRef(null);
-  const [dropDownPos, setDropDownPos] = useState({ top: 0, right:70 });
+  const [dropDownPos, setDropDownPos] = useState({ top: 0, right: 70 });
 
   useEffect(() => {
     if (isProfileMenuOpen && profileButtonRef.current) {
@@ -51,73 +51,55 @@ const ProfileComponent = () => {
           >
             {/* Desktop Dropdown */}
             <div className="hidden md:flex flex-col transition-all">
-              <Button
-                width="w-20"
-                height="h-[30px]"
-                background="bg-gray-300"
-                label="Profile"
-                margin="mt-1"
-                dark="dark:bg-amber-400"
-              />
-              <Button
-                width="w-20"
-                height="h-[30px]"
-                background="bg-gray-300"
-                label="Login"
-                margin="mt-1"
-                dark="dark:bg-amber-400"
-              />
-              <Button
-                width="w-20"
-                height="h-[30px]"
-                background="bg-gray-300"
-                label="Signup"
-                margin="mt-1"
-                dark="dark:bg-amber-400"
-              />
+              {[
+                { label: "Profile" },
+                { label: "Login" },
+                {
+                  label: "Sign Up",
+                },
+              ].map((item, index) => (
+                <Button
+                  key={index}
+                  width="w-20"
+                  height="h-[30px]"
+                  background="bg-gray-300"
+                  label={item.label}
+                  margin="mt-1"
+                  dark="dark:bg-amber-400"
+                />
+              ))}
             </div>
 
             {/* Mobile Dropdown */}
             <div className="flex md:hidden flex-col transition-all">
-              <div className="relative group">
-                <Button
-                  width="w-10"
-                  height="h-10"
-                  icon={<GiCaptainHatProfile className="text-2xl" />}
-                  background="text-neutral-900"
-                  dark="dark:text-amber-500"
-                  margin="mr-5"
-                />
-                <span className="absolute left-full top-1/5 -translate-y-1/2 ml-1 px-1 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-10">
-                  Profile
-                </span>
-              </div>
-              <div className="relative group">
-                <Button
-                  width="w-10"
-                  height="h-10"
-                  icon={<SiSimplelogin className="text-2xl" />}
-                  background="text-neutral-900"
-                  dark="dark:text-amber-500"
-                  margin="mr-5"
-                />
-                <span className="absolute left-full top-1/5 -translate-y-1/2 ml-1 px-1 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-10">
-                  Login
-                </span>
-              </div>
-              <div className="relative group">
-                <Button
-                  width="w-10"
-                  height="h-10"
-                  icon={<SiGnuprivacyguard className="text-2xl" />}
-                  background="text-neutral-900"
-                  dark="dark:text-amber-500"
-                  margin="mr-5"
-                />
-                <span className="absolute left-full top-1/5 -translate-y-1/2 ml-1 px-1 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-10">
-                  Signup
-                </span>
-              </div>
+              {[
+                {
+                  icon: <GiCaptainHatProfile className="text-2xl" />,
+                  label: "Profile",
+                },
+                {
+                  icon: <SiSimplelogin className="text-2xl" />,
+                  label: "Login",
+                },
+                {
+                  icon: <SiGnuprivacyguard className="text-2xl" />,
+                  label: "Sign UP",
+                },
+              ].map((item, index) => (
+                <div className="relative group" key={index}>
+                  <Button
+                    width="w-10"
+                    height="h-10"
+                    background="text-neutral-900"
+                    dark="dark:text-amber-500"
+                    margin="mr-5"
+                    icon={item.icon}
+                  />
+                  <span className="absolute left-full top-1/5 -translate-y-1/2 ml-1 px-1 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-10">
+                    {item.label}
+                  </span>
+                </div>
+              ))}
             </div>
           </div>
         )}
